fed648ef683a4efd477bc2c650991cacf8f4aae7
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-01-28 23:30:37 +0900

    Add local storage util file

---
+++ static/template/src/util/storage.js
@@ -0,0 +1,16 @@
+// TODO: Change this
+const keyPrefix = 'my-web-game'
+
+const { localStorage } = window
+
+const prefixKey = (key) => `${keyPrefix}.${key}`
+
+export const save = (key, obj) => {
+  localStorage.setItem(prefixKey(key), JSON.stringify(obj))
+}
+
+export const restore = (key) => JSON.parse(localStorage.getItem(prefixKey(key)) || null)
+
+export const remove = (key) => {
+  localStorage.removeItem(prefixKey(key))
+}
