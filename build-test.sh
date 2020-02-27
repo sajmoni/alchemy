4e47a01156fc7f57bdc18c699397c3b17496c279
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-02-27 20:05:41 +0900

    Improve build-test command

---
+++ build-test.sh
@@ -0,0 +1,15 @@
+set -e
+
+runCommand() {
+  echo "=== $1 ==="
+  $1
+}
+
+runCommand "yarn clean"
+runCommand "yarn build"
+runCommand "yarn pack --filename make-web-game.tgz"
+runCommand "cd example"
+runCommand "yarn refresh"
+runCommand "yarn make-web-game test-game"
+runCommand "cd test-game"
+runCommand "yarn start"
