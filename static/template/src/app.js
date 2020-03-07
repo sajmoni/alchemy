970bd5ce6987a7cfb1684237df8f5d4ead90b468
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-03-07 22:10:19 +0900

    Template: Update Pixi settings

--- static/template/src/app.js
+++ static/template/src/app.js
@@ -1,15 +1,20 @@
 import * as PIXI from 'pixi.js'
 import { GAME_HEIGHT, GAME_WIDTH } from './constant'
 
-// * Enable to turn on pixel perfect rendering
+// * Enable to turn on pixel perfect rendering:
 // PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
+// PIXI.settings.ROUND_PIXELS = true
 
 const renderer = new PIXI.Renderer({
   width: GAME_WIDTH,
   height: GAME_HEIGHT,
-  antialias: false,
+  // * Enable to turn on pixel perfect rendering:
+  // antialias: false,
   // * Can be enabled once a background image exists
+  // * This might increase performance
   // clearBeforeRender: false,
+  // preserveDrawingBuffer: true,
+  // 
   backgroundColor: 0x000077,
 })
 
