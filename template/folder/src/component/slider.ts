42d5d04628ca26a6f06f6073a3345ea8cabaadbc
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-05-24 22:26:47 +0900

    Template: Update spritesheet

--- template/folder/src/component/slider.ts
+++ template/folder/src/component/slider.ts
@@ -16,7 +16,7 @@ export default ({
 }: arguments): [PIXI.Container, render] => {
   const component = new PIXI.Container()
 
-  const minus = new PIXI.Sprite(ex.getTexture('plus-minus-0'))
+  const minus = new PIXI.Sprite(ex.getTexture('plus-minus-1'))
   minus.x = -50
   minus.anchor.set(0.5)
   ex.makeClickable(minus, onMinus)
@@ -26,7 +26,7 @@ export default ({
   text.anchor.set(0.5)
   component.addChild(text)
 
-  const plus = new PIXI.Sprite(ex.getTexture('plus-minus-1'))
+  const plus = new PIXI.Sprite(ex.getTexture('plus-minus-2'))
   plus.anchor.set(0.5)
   ex.makeClickable(plus, onPlus)
   plus.x = 50
