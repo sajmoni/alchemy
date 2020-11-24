f8390d95ac47ff3b32a25aff134c1a92a244e25d
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-11-24 17:06:30 +0100

    Convert loading.js to typescript

--- template/folder/src/util/loading.ts
+++ template/folder/src/util/loading.ts
@@ -4,7 +4,14 @@ const loading = document.createElement('div')
 loading.innerHTML = 'Please wait'
 loading.style.fontFamily = 'Helvetica'
 loading.setAttribute('id', 'loading')
-document.querySelector('#game').append(loading)
+
+const SELECTOR = '#game'
+const element = document.querySelector(SELECTOR)
+if (!element) {
+  throw new Error(`Couldn't find element: ${SELECTOR}`)
+}
+
+element.append(loading)
 
 document.addEventListener('DOMContentLoaded', () => {
   loading.remove()
