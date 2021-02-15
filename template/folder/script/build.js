1f2ce7f44f8e386c5c942d1c4ed977265cab7a23
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-02-15 20:00:12 +0100

    Log game size on build

--- template/folder/script/build.js
+++ template/folder/script/build.js
@@ -16,6 +16,35 @@ const buildOptions = {
   },
 }
 
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) {
+    return '0 Bytes'
+  }
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return Number.parseFloat((bytes / k ** i).toFixed(dm)) + ' ' + sizes[i]
+}
+
+const calculateSize = (path) => {
+  if (fs.lstatSync(path).isFile()) {
+    return fs.statSync(path).size
+  }
+
+  if (fs.lstatSync(path).isDirectory()) {
+    // eslint-disable-next-line unicorn/no-array-reduce
+    return fs.readdirSync(path).reduce((total, localPath) => {
+      return total + calculateSize(`${path}/${localPath}`)
+    }, 0)
+  }
+
+  throw new Error('Error when calculating game size')
+}
+
 const run = async () => {
   try {
     await build(buildOptions)
@@ -28,10 +57,14 @@ const run = async () => {
       fs.copy('public/index.html', `${buildOptions.outdir}/index.html`),
     ])
 
+    const size = calculateSize(buildOptions.outdir)
+
     console.log()
     console.log(chalk.green(`   Success!`))
     console.log()
     console.log(`   Built to ${chalk.cyan(buildOptions.outdir)}`)
+    console.log()
+    console.log(`   ${chalk.gray(`Game size:`)} ${formatBytes(size)}`)
     console.log()
   } catch (error) {
     console.log(chalk.red(`Could not copy public files: ${error}`))
