8d1118b1a6e2020e6cc191bb4841e37e6768c375
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-02-25 20:38:11 +0100

    Check that state path exists when rerendering in development

--- template/folder/src/init/sceneHandler.ts
+++ template/folder/src/init/sceneHandler.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js'
 import * as prism from 'state-prism'
+import dotProp from 'dot-prop'
 
 /* PLOP_INJECT_IMPORT */
 import mainMenu from '/scene/mainMenu'
@@ -8,11 +9,9 @@ import { Scene } from '/enum'
 import app from '/app'
 import { RenderScene, SceneArgs } from '/type/scene'
 import state from '/state'
+import env from '/env'
 
-const sceneHandler: Record<
-  Scene,
-  (sceneArgs: SceneArgs) => RenderScene | void
-> = {
+const sceneHandler: Record<Scene, (sceneArgs: SceneArgs) => RenderScene> = {
   /* PLOP_INJECT_SCENE */
   [Scene.MAIN_MENU]: mainMenu,
   [Scene.GAME]: game,
@@ -39,12 +38,16 @@ const initializeSceneHandler = (): void => {
       container,
     })
 
-    if (render) {
-      Object.entries(render).forEach(([path, subscribe]) => {
-        const unsubscribe = prism.subscribe(path, subscribe)
-        unsubscribeFunctions.push(unsubscribe)
-      })
-    }
+    Object.entries(render).forEach(([path, subscribe]) => {
+      if (env.NODE_ENV === 'development' && !dotProp.has(state, path)) {
+        console.error(
+          `SceneHandler: Path "${path}" was returned from scene "${scene}" but does not exist in state`,
+        )
+      }
+
+      const unsubscribe = prism.subscribe(path, subscribe)
+      unsubscribeFunctions.push(unsubscribe)
+    })
   }
 
   prism.subscribe('scene', loadScene)
