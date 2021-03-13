33b8935beefbee1713472da60dd727d49c5b25f2
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-03-13 19:22:12 +0100

    Remove redundant plop data generator

--- template/folder/plop/plopfile.ts
+++ template/folder/plop/plopfile.ts
@@ -1,7 +1,6 @@
 import ui from './ui'
 import scene from './scene'
 import enumGenerator from './enum'
-import data from './data'
 import lab from './lab'
 import effect from './effect'
 import particles from './particles'
@@ -10,7 +9,6 @@ module.exports = (plop): void => {
   plop.setGenerator('UI component', ui)
   plop.setGenerator('Scene', scene)
   plop.setGenerator('Enum', enumGenerator)
-  plop.setGenerator('Data', data)
   plop.setGenerator('Lab', lab)
   plop.setGenerator('Effect', effect)
   plop.setGenerator('Particles', particles)
