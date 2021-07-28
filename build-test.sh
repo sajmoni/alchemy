fc6cf036c8f467d65be5c25e9d5caf22b95dd57d
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-07-28 10:01:13 +0200

    Use package-preview

--- build-test.sh
+++ build-test.sh
@@ -8,10 +8,8 @@ runCommand() {
 
 runCommand "yarn clean"
 runCommand "yarn build"
-runCommand "yarn pack --filename make-web-game.tgz"
-runCommand "cd example"
-runCommand "yarn refresh"
+runCommand "yarn preview"
 runCommand "yarn make-web-game test-game"
 runCommand "cd test-game"
-runCommand "git remote add origin https://github.com/sajmoni/make-web-game-test-repo.git"
+# runCommand "git remote add origin https://github.com/sajmoni/make-web-game-test-repo.git"
 runCommand "yarn start"
