bfcf7956b5a02aefd4202f358e08a802b561d681
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-12-28 10:35:46 +0100

    Simplify getTexture

--- template/folder/src/util/getTexture.ts
+++ template/folder/src/util/getTexture.ts
@@ -2,19 +2,7 @@ import { getTexture as getTextureEx } from 'pixi-ex'
 
 import textureData from '~/public/asset/spritesheet/data.json'
 
-// TODO: Move to tiny-toolkit.
-
-/**
- * CreateTypeFromObject<typeof anyObject>
- * Only the keys:
- * CreateTypeFromObject<keyof typeof anyObject>
- */
-type CreateTypeFromObject<T> = {
-  [P in keyof T]: T[P] extends string ? T[P] : never
-}
-
-const getTexture = (
-  textureName: CreateTypeFromObject<keyof typeof textureData.frames>,
-) => getTextureEx(textureName)
+const getTexture = (textureName: keyof typeof textureData.frames) =>
+  getTextureEx(textureName)
 
 export default getTexture
--- template/folder/src/util/getTextures.ts
+++ template/folder/src/util/getTextures.ts
@@ -2,19 +2,7 @@ import { getTextures as getTexturesEx } from 'pixi-ex'
 
 import textureData from '~/public/asset/spritesheet/data.json'
 
-// TODO: Move to tiny-toolkit.
-
-/**
- * CreateTypeFromObject<typeof anyObject>
- * Only the keys:
- * CreateTypeFromObject<keyof typeof anyObject>
- */
-type CreateTypeFromObject<T> = {
-  [P in keyof T]: T[P] extends string ? T[P] : never
-}
-
-const getTextures = (
-  textureNames: Array<CreateTypeFromObject<keyof typeof textureData.frames>>,
-) => getTexturesEx(textureNames)
+const getTextures = (textureNames: Array<keyof typeof textureData.frames>) =>
+  getTexturesEx(textureNames)
 
 export default getTextures
