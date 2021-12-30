8f4e00088395b5c9fac228d48408a94d3049bd65
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-12-30 11:44:48 +0100

    Include png files and subfolders in sprite sheet generation script

--- template/folder/script/aseprite.sh
+++ template/folder/script/aseprite.sh
@@ -1,6 +1,6 @@
 set -e
 
 alias aseprite="$HOME/Library/Application\ Support/Steam/steamapps/common/Aseprite/Aseprite.app/Contents/MacOS/aseprite"
-aseprite -b sprite/*.ase --sheet src/public/asset/spritesheet/sheet.png --data src/public/asset/spritesheet/data.json --filename-format '{title}-{frame1}'
+aseprite -b sprite/**/*.{png,ase} sprite/*.{png,ase} --sheet src/public/asset/spritesheet/sheet.png --data src/public/asset/spritesheet/data.json --filename-format '{title}-{frame1}'
 prettier --write src/public/asset/spritesheet/data.json
 echo "Spritesheet generated!"
