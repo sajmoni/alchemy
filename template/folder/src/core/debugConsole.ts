a979121b7e33c09f8a52c3b79a30988f46de9045
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-11-11 00:01:06 +0100

    Simplify debug console

--- template/folder/src/core/debugConsole.ts
+++ template/folder/src/core/debugConsole.ts
@@ -3,27 +3,12 @@ import * as ex from 'pixi-ex'
 import { snapshot } from 'valtio'
 
 import app from '~/app'
-import state, { State } from '~/state'
+import state from '~/state'
 import Sound from '~/sound'
-
-type ConsoleInfo = Record<string, any>
-
-type Debug = {
-  state: () => State
-  info: () => ConsoleInfo
-  sound: () => void
-}
-
-declare global {
-  interface Window {
-    debug: Debug
-  }
-}
 
 const initializeDebugConsole = () => {
   // * These commands can be run in the console, e.g: 'debug.state()'
-  window.debug = {
-    ...window.debug,
+  ;(window as any).debug = {
     state: () => snapshot(state),
     info: () => ({
       'display objects': ex.getAllChildren(app.stage).length,
