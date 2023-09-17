5950fca23931c1e265901102c4e5d20c90c88d7f
Simon Lagos <simon.lagos.sallhed@gmail.com>
2023-09-17 18:13:49 +0200

    Add a loadDataFromImage function

---
+++ src/lib/module/loadDataFromImage.ts
@@ -0,0 +1,45 @@
+import { times2d } from 'tiny-toolkit'
+
+// R-G-B
+export type Pixel = `${number}-${number}-${number}`
+
+/**
+ * This function can be used to for example load a level from image data
+ */
+export default async function loadDataFromImage(imagePath: string): Promise<{
+  pixels: Pixel[]
+  width: number
+  height: number
+}> {
+  return new Promise((resolve) => {
+    let img = new window.Image()
+    img.src = imagePath
+    img.onload = () => {
+      const canvas = document.createElement('canvas')
+      canvas.width = img.width
+      canvas.height = img.height
+      const context2d = canvas.getContext('2d') as CanvasRenderingContext2D
+      context2d.drawImage(img, 0, 0)
+      const { data } = context2d.getImageData(0, 0, canvas.width, canvas.height)
+      let pixelArray: Pixel[] = []
+
+      times2d(canvas.width, canvas.height, (x, y) => {
+        const pixel = canvas.width * x + y
+        const position = pixel * 4
+
+        const red = data[position]!
+        const green = data[position + 1]!
+        const blue = data[position + 2]!
+        // Ignore alpha
+        // const alpha = data[position+3]!
+        pixelArray.push(`${red}-${green}-${blue}`)
+      })
+
+      resolve({
+        pixels: pixelArray,
+        width: canvas.width,
+        height: canvas.height,
+      })
+    }
+  })
+}
