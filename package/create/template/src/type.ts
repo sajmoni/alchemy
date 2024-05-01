663c3923e54503b24ef3b7cd05b5244efbad13c9
Simon Lagos <simon.lagos.sallhed@gmail.com>
2024-05-01 23:04:37 +0200

    Include alchemy state in state type in template

--- package/create/template/src/type.ts
+++ package/create/template/src/type.ts
@@ -1,4 +1,4 @@
-import { type BaseScene } from 'alchemy-engine'
+import { type BaseScene, type AlchemyState } from 'alchemy-engine'
 
 import type textureData from '~/public/asset/spritesheet/data.json'
 import sounds from './sounds.json'
@@ -6,7 +6,9 @@ import { keys, scenes, state } from './data'
 
 export type Keys = typeof keys
 export type TextureName = keyof typeof textureData.frames
-export type State = typeof state
+export type State = typeof state & {
+  alchemy: AlchemyState<SceneKey>
+}
 export type SceneKey = keyof typeof scenes
 export type SoundName = keyof typeof sounds.sound
 export type MusicName = keyof typeof sounds.music
