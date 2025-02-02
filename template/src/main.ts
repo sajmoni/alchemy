efd98877dfc544f3ab5da179326799a1dba6cb3d
Simon Lagos <simon.lagos.sallhed@gmail.com>
2025-02-02 15:47:51 +0100

    Template: Avoid top-level await due to rollup issue

--- template/src/main.ts
+++ template/src/main.ts
@@ -7,35 +7,39 @@ import sounds from './sounds.json'
 
 const app = new Application()
 
-await app.init({
-  width: 640,
-  height: 480,
-  resolution: 2,
-  antialias: true,
-})
+async function main() {
+  await app.init({
+    width: 640,
+    height: 480,
+    resolution: 2,
+    antialias: true,
+  })
 
-const spriteSheetPath = './asset/spritesheet/data.json'
-const font = '10pt "Press Start 2P"'
+  const spriteSheetPath = './asset/spritesheet/data.json'
+  const font = '10pt "Press Start 2P"'
 
-createGame({
-  app,
-  state,
-  scene: 'mainMenu',
-  scenes,
-  keys,
-  sounds,
-  spriteSheetPath,
-  font,
-  config: {
-    pixelPerfect: true,
-  },
-  panel: [
-    {
-      type: 'string',
-      label: 'gold',
-      getValue: (state) => {
-        return state.gold.toString()
-      },
+  createGame({
+    app,
+    state,
+    scene: 'game',
+    scenes,
+    keys,
+    sounds,
+    spriteSheetPath,
+    font,
+    config: {
+      pixelPerfect: true,
     },
-  ],
-})
+    panel: [
+      {
+        type: 'string',
+        label: 'gold',
+        getValue: (state) => {
+          return state.gold.toString()
+        },
+      },
+    ],
+  })
+}
+
+main()
