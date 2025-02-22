7f078b0b5f3c447d27368f0e8d2bfb23abb4356c
Simon Lagos <simon.lagos.sallhed@gmail.com>
2025-02-22 22:52:01 +0100

    Update screen shake

--- src/runtime/internal/useScreenShake.ts
+++ src/runtime/internal/useScreenShake.ts
@@ -12,21 +12,12 @@ export default function createUseScreenShake(timer: Timer) {
     const speed = screenShakeOptions.speed || 1
     const screenShake = createScreenShake(screenShakeOptions)
 
-    // We remove the previous screen shake to make it work with a non-static camera
-    let previousScreenShake:
-      | { angle: number; offsetX: number; offsetY: number }
-      | undefined
-
     timer.repeatEvery(1, (time) => {
       const { angle, offsetX, offsetY } = screenShake.update(time * speed)
 
-      camera.angle = camera.angle + angle - (previousScreenShake?.angle ?? 0)
-      camera.position.x =
-        camera.x + offsetX - (previousScreenShake?.offsetX ?? 0)
-      camera.position.y =
-        camera.y + offsetY - (previousScreenShake?.offsetY ?? 0)
-
-      previousScreenShake = { angle, offsetX, offsetY }
+      camera.angle = camera.angle + angle
+      camera.position.x = camera.x + offsetX
+      camera.position.y = camera.y + offsetY
     })
 
     /**
