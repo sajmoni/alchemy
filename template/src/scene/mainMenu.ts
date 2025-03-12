def1a005171a7fb0441ebf2828075ea0ce676023
Simon Lagos <simon.lagos.sallhed@gmail.com>
2025-03-12 20:55:12 +0100

    Template: Improve main menu

--- template/src/scene/mainMenu.ts
+++ template/src/scene/mainMenu.ts
@@ -13,17 +13,15 @@ export default async function mainMenu(scene: Scene) {
     { ...TextStyle.MAIN, fontSize: 50 },
     packageJson.name,
   )
-  titleText.label = 'titleText'
-  titleText.anchor.set(0.5)
-  titleText.x = app.screen.width / 2
-  titleText.y = app.screen.height / 3
+  titleText.anchor = 0.5
+  titleText.position = { x: app.screen.width / 2, y: app.screen.height / 3 }
 
   animate.sine({
     onUpdate: (value) => {
       titleText.angle = value
     },
-    startValue: titleText.angle - 7,
-    endValue: titleText.angle + 7,
+    startValue: titleText.angle - 3,
+    endValue: titleText.angle + 3,
     duration: 280,
   })
 
@@ -32,53 +30,26 @@ export default async function mainMenu(scene: Scene) {
     { ...TextStyle.MAIN, fontSize: 20 },
     'Start game',
   )
-  startGameButton.label = 'startGameButton'
-  startGameButton.anchor.set(0.5)
-  startGameButton.position.y = app.screen.height - 140
-  startGameButton.position.x = app.screen.width / 2
+  startGameButton.anchor = 0.5
+  startGameButton.position = {
+    x: app.screen.width / 2,
+    y: app.screen.height - 140,
+  }
 
   onClick(startGameButton, () => {
     setScene('game')
   })
-
-  // TODO: onHoverAnimation which includes cancel behavior built-in?
-  let cancel: (() => void) | undefined
-  const hoverAnimation = (container: Container) => ({
-    onOver: () => {
-      const startValue = 1
-      const promise = animate.easeOut({
-        onUpdate: (value) => {
-          container.scale.set(value)
-        },
-        startValue,
-        endValue: startValue + 0.18,
-        duration: 15,
-      })
-
-      cancel = promise.cancel
-    },
-    onOut: () => {
-      if (cancel) {
-        cancel()
-      }
-      container.scale.set(1)
-    },
-  })
-
-  onHover(startGameButton, hoverAnimation(startGameButton))
 
   const settingsButton = text(
     container,
     { ...TextStyle.MAIN, fontSize: 20 },
     'Settings',
   )
-  settingsButton.label = 'settingsButton'
-  settingsButton.anchor.set(0.5)
-  settingsButton.position.y = app.screen.height - 100
-  settingsButton.position.x = app.screen.width / 2
-
-  onHover(settingsButton, hoverAnimation(settingsButton))
-
+  settingsButton.anchor = 0.5
+  settingsButton.position = {
+    x: app.screen.width / 2,
+    y: app.screen.height - 100,
+  }
   onClick(settingsButton, () => {
     state.settingsVisible = true
   })
